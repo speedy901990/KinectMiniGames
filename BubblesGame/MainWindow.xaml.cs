c8691d4acc295d0ec5fbfd20dbd5bb25e0b7efc8
slk187 <kinga123>
2013-07-06 18:43:33 +0200

    Minor changes and very very basis connection with Kinect added to BubbleGame

--- BubblesGame/MainWindow.xaml.cs
+++ BubblesGame/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Kinect;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BubblesGame
 {
@@ -20,9 +9,77 @@ namespace BubblesGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Member Variables
+        private KinectSensor _kinect;
+        #endregion Member Variables
+
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += (s, e) => DiscoverKinectSensor();
+            Unloaded += (s, e) => { Kinect = null; };
         }
+        #endregion Constructor
+
+        #region Methods
+        private void DiscoverKinectSensor()
+        {
+            KinectSensor.KinectSensors.StatusChanged += KinectSensors_StatusChanged;
+            Kinect = KinectSensor.KinectSensors
+                                      .FirstOrDefault(x => x.Status == KinectStatus.Connected);
+        }
+
+        private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case KinectStatus.Connected:
+                    if (Kinect == null)
+                    {
+                        Kinect = e.Sensor;
+                    }
+                    break;
+                case KinectStatus.Disconnected:
+                    if (Kinect == e.Sensor)
+                    {
+                        Kinect = null;
+                        Kinect = KinectSensor.KinectSensors
+                                      .FirstOrDefault(x => x.Status == KinectStatus.Connected);
+                        if (Kinect == null)
+                        {
+                            //Notify the user that the sensor is disconnected
+                        }
+                    }
+                    break;
+
+                //Handle all other statuses according to needs
+            }
+        }
+        #endregion Methods
+
+        #region Properties
+        public KinectSensor Kinect
+        {
+            get { return _kinect; }
+            set
+            {
+                if (_kinect != value)
+                {
+                    if (_kinect != null)
+                    {
+                        //Uninitialize
+                        _kinect = null;
+                    }
+                    if (value != null && value.Status == KinectStatus.Connected)
+                    {
+                        _kinect = value;
+                        //Initialize
+                    }
+                }
+            }
+        }
+        #endregion Properties
     }
 }
