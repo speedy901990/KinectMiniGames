d28313a3de105ce9e8c8371ebfe372f67b9fc457
Grzesiek <grzesiekmos@gmail.com>
2013-07-23 19:24:20 +0200

    Wykrywanie ruchu

---
+++ ApplesGame/Apple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplesGame
+{
+    public class Apple
+    {
+        //pozycja jablka
+        private int posX;
+        private int posY;
+
+        //rozmiar
+        private int size;
+
+        //konstruktor tworzacy jablko
+        public Apple(int xMin, int xMax, int yMin, int yMax)
+        {
+            System.Random rand = new Random(Seed);
+            rand.Next(xMin, xMax);
+            setPosition(rand.Next(xMin, xMax), rand.Next(yMin, yMax));
+        }
+        public void setPosition(int x, int y)
+        {
+            this.posX = x;
+            this.posY = y;
+        }
+
+        public int Seed { get; set; }
+    }
+}
--- ApplesGame/MainWindow.xaml.cs
+++ ApplesGame/MainWindow.xaml.cs
@@ -1,4 +1,21 @@
-﻿using System.Windows;
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit.Controls;
+using Microsoft.Kinect.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
 
 namespace ApplesGame
 {
@@ -7,9 +24,93 @@ namespace ApplesGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        //ilosc jablek na ekranie
+        public int applesCount = 1;
+
+        private KinectSensorChooser sensorChooser;
+
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            this.sensorChooser = new KinectSensorChooser();
+            this.sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+            this.sensorChooserUI.KinectSensorChooser = this.sensorChooser;
+            this.sensorChooser.Start();
+            var regionSensorBinding = new Binding("Kinect") { Source = this.sensorChooser };// jeśli Kinect jest pod Xbox to potrzebne te dwie linijki (1)
+            BindingOperations.SetBinding(this.kinectRegion, Microsoft.Kinect.Toolkit.Controls.KinectRegion.KinectSensorProperty, regionSensorBinding);// inaczej nie wykrywa ruchu  (2)
+        }
+
+        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs args)      // poprawne uruchamianie oraz włączanie głębi i szkieletu w Kinect 
+        {
+            bool error = false;
+            if (args.OldSensor != null)
+            {
+                try
+                {
+                    args.OldSensor.DepthStream.Range = Microsoft.Kinect.DepthRange.Default;
+                    args.OldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    args.OldSensor.DepthStream.Disable();
+                    args.OldSensor.SkeletonStream.Disable();
+                }
+                catch (InvalidOperationException)
+                {
+                    // Kinect moze przejsc w nieprawidlowy stan podczas wlaczania/wylaczania 
+                    // Np. moze byc nagle wylaczony
+                    error = true;
+                }
+            }
+
+            if (args.NewSensor != null)
+            {
+                try
+                {
+                    args.NewSensor.DepthStream.Enable(Microsoft.Kinect.DepthImageFormat.Resolution640x480Fps30);
+                    args.NewSensor.SkeletonStream.Enable();
+
+                    try
+                    {
+                        args.NewSensor.DepthStream.Range = Microsoft.Kinect.DepthRange.Near;
+                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                        args.NewSensor.SkeletonStream.TrackingMode = Microsoft.Kinect.SkeletonTrackingMode.Seated;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // Non Kinect for Windows devices do not support Near mode, so reset back to default mode.
+                        args.NewSensor.DepthStream.Range = Microsoft.Kinect.DepthRange.Default;
+                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                        error = true;
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                    error = true;
+
+                }
+            }
+
+
+        }
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.sensorChooser.Stop();
+        }
+        /// <summary>
+        /// Handle a button click from the wrap panel.
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        private void ButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Well done!");
         }
     }
 }
