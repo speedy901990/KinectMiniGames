4bdc3c9219a9e73692def46679743335341147a2
grzesiekmos <grzesiekmos@gmail.com>
2014-06-03 13:23:45 +0200

    bitmapy sa ladowane z resourcow
    fixes #31

---
+++ BubblesGame/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+namespace BubblesGame
+{
+    class Utilities
+    {
+        public static BitmapSource convertBitmapToBitmapSource(Bitmap bm)
+        {
+            var bitmap = bm;
+            var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            bitmap.Dispose();
+            return bitmapSource;
+        }
+    }
+}
