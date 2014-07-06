dee61570adce8b9dfb282c6060ef8367826ccc30
Grzegorz Paśnik <grzesiekmos@gmail.com>
2014-07-06 14:05:14 +0200

    obsluga ekranu konfiguracyjnego jablek i baniek kinectem, zmiana w bankach ze sliderow na buttony i poziomy trudnosci

--- KinectMiniGames/ConfigPages/ApplesGameConfigPage.xaml.cs
+++ KinectMiniGames/ConfigPages/ApplesGameConfigPage.xaml.cs
@@ -27,37 +27,55 @@ namespace KinectMiniGames
             parent.Children.Remove(this);
         }
 
-        private void submitApplesConfig_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void kcbLevel1_Click(object sender, RoutedEventArgs e)
         {
             if (Application.Current.MainWindow.Width < 1440)
                 Config.TreesCount = 2;
             else
                 Config.TreesCount = 3;
 
-            if (rbLvl1.IsChecked == true)
-            {
-                Config.ApplesOnTreeCount = 4;
-                Config.ColorCount = 3;
-                Config.BasketCount = 3;
-            }
-            else if (rbLvl2.IsChecked == true)
-            {
-                Config.ApplesOnTreeCount = 6;
-                Config.ColorCount = 4;
-                Config.BasketCount = 4;
-            }
-            else if (rbLvl3.IsChecked == true)
-            {
-                Config.ApplesOnTreeCount = 10;
-                Config.ColorCount = 5;
-                Config.BasketCount = 6;
-            }
+            Config.ApplesOnTreeCount = 4;
+            Config.ColorCount = 3;
+            Config.BasketCount = 3;
+
             Config.PassedKinectSensorChooser = this.kinectSensor;
             ApplesGame.MainWindow window = new ApplesGame.MainWindow(Config);
             window.Show();
         }
 
-        private void btnBackToMenu_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void kcbLevel2_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.Width < 1440)
+                Config.TreesCount = 2;
+            else
+                Config.TreesCount = 3;
+
+            Config.ApplesOnTreeCount = 6;
+            Config.ColorCount = 4;
+            Config.BasketCount = 4;
+
+            Config.PassedKinectSensorChooser = this.kinectSensor;
+            ApplesGame.MainWindow window = new ApplesGame.MainWindow(Config);
+            window.Show();
+        }
+
+        private void kcbLevel3_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.Width < 1440)
+                Config.TreesCount = 2;
+            else
+                Config.TreesCount = 3;
+
+            Config.ApplesOnTreeCount = 10;
+            Config.ColorCount = 5;
+            Config.BasketCount = 6;
+
+            Config.PassedKinectSensorChooser = this.kinectSensor;
+            ApplesGame.MainWindow window = new ApplesGame.MainWindow(Config);
+            window.Show();
+        }
+
+        private void ktbBackToMenu_Click(object sender, RoutedEventArgs e)
         {
             var parent = (Panel)this.Parent;
             parent.Children.Remove(this);
